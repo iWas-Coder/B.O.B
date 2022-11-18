9c36a3685bf5d8ed2a04016169893d8ffdc8387f
iWas-Coder <wasymatieh01@gmail.com>
2022-11-18 22:29:08 +0100

    Script redisign

--- chapters/chapter03.sh
+++ chapters/chapter03.sh
@@ -6,24 +6,39 @@
 ####################################
 # CHAPTER 03: Packages and Patches #
 ####################################
+banner "CHAPTER 03: Packages and Patches"; separator
 
 # $LFS/sources can be used both as the place to store the tarballs and patches and as a working directory.
 # To create this directory, execute the following command, as user root, before starting the download session:
-sudo mkdir $LFS/sources
+if [ ! -d "$LFS"/sources ]; then sudo mkdir "$LFS"/sources; fi
 
 # Make this directory writable and sticky.
 # “Sticky” means that even if multiple users have write permission on a directory, only the owner of a file can delete the file within a sticky directory.
 # The following command will enable the write and sticky modes:
-sudo chmod a+wt $LFS/sources
+sudo chmod a+wt "$LFS"/sources
 
 # To download all of the packages and patches use:
-for pkg in $(ls ../sources/sources.list); do
-  wget -nc "$pkg" -P $LFS/sources
+banner "Downloading sources in $LFS/sources"; separator
+for pkg in $(cat "$BOB"/sources/sources.list); do
+    wget -nc "$pkg" -P "$LFS"/sources
 done
+separator
 
-# Checking checksums (md5, sha1, sha256, sha384, sha512) for all sources downloaded previously at $LFS/sources.
-# md5sum -c sources.md5
-# sha1sum -c sources.sha1
-# sha256sum -c sources.sha256
-# sha384sum -c sources.sha384
-# sha512sum -c sha512
+# Checking checksums (md5, sha1, sha256, sha384, sha512) for all sources downloaded previously at $LFS/sources:
+pushd "$LFS"/sources &>/dev/null
+banner "Checking MD5 of downloaded sources"; separator
+md5sum -c "$BOB"/sources/sources.md5
+separator
+banner "Checking SHA1 of downloaded sources"; separator
+sha1sum -c "$BOB"/sources/sources.sha1
+separator
+banner "Checking SHA256 of downloaded sources"; separator
+sha256sum -c "$BOB"/sources/sources.sha256
+separator
+banner "Checking SHA384 of downloaded sources"; separator
+sha384sum -c "$BOB"/sources/sources.sha384
+separator
+banner "Checking SHA512 of downloaded sources"; separator
+sha512sum -c "$BOB"/sources/sha512
+separator
+popd &>/dev/null
---
+++ chapters/envsetup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Source: https://www.linuxfromscratch.org/lfs/view/stable-systemd
+# Author: Wasym Atieh Alonso
+
+############################
+# Script Environment Setup #
+############################
+# Hide cursor
+tput civis; clear
+# Colors for pretty-printing
+C_RESET='\033[0m'
+C_YELLOW='\033[1;33m'
+C_RED='\033[1;31m'
+# Functions for pretty-printing
+ctrl_c () {
+    echo -e "\n\n${C_RED}[-] Process cancelled by user. Exiting...\n${C_RESET}"
+    tput cnorm; exit 1
+}
+trap ctrl_c INT
+separator () {
+    echo -ne "$C_RED"
+    printf '%.s─' $(seq 1 $(tput cols))
+    echo -ne "$C_RESET"
+    sleep 2
+}
+banner () {
+    echo -e "${C_YELLOW}[*] $1:${C_RESET}"
+}
+confirm () {
+    echo -ne "${C_YELLOW}Still alive? Continue? (y/n) ${C_RESET}"
+    read -n 1 -r
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]; then ctrl_c; fi
+    separator
+}
+# Sudo privileges
+sudo whoami &>/dev/null
+# Base directories
+BOB=$(pwd)
+LFS="/mnt/lfs"
+if [ ! -d "$LFS" ]; then sudo mkdir "$LFS"; fi
+banner "Script Environment Setup"; separator
+echo "BOB: $BOB"
+echo "LFS: $LFS"
+echo
