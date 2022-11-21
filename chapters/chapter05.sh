5fc0cbdbf26623289bc37069a3d203a39d941840
iWas-Coder <wasymatieh01@gmail.com>
2022-11-21 18:00:00 +0100

    Developing chapter 5

--- chapters/chapter05.sh
+++ chapters/chapter05.sh
@@ -11,13 +11,18 @@ banner "CHAPTER 05: Compiling a Cross-Toolchain"; separator
 cd $LFS/sources
 
 # 5.2.1. Installation of Cross Binutils
-source chapter05/binutils-pass1.sh
+source chapter05/5.2-binutils-pass1.sh
 
 # 5.3.1. Installation of Cross GCC
-source chapter05/gcc-pass1.sh
+source chapter05/5.3-gcc-pass1.sh
 
 # 5.4.1. Installation of Linux API Headers
-source chapter05/linux-api-headers.sh
+source chapter05/5.4-linux-api-headers.sh
 
 # 5.5.1. Installation of Glibc
-source chapter05/glibc.sh
+source chapter05/5.5-glibc.sh
+
+# 5.6.1. Installation of Target Libstdc++
+source chapter05/5.6-libstdc++.sh
+
+separator
---
+++ chapters/chapter05/5.6-libstdc++.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Source: https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter05/gcc-libstdc++.html
+# Author: Wasym Atieh Alonso
+
+###########################################
+# CHAPTER 05: Compiling a Cross-Toolchain #
+###########################################
+# 5.6.1. Installation of Target Libstdc++
+# Approximate build time: 0.4 SBU
+# Required disk space:    1.1 GB
+
+tar -xvf gcc*
+cd gcc*/
+# Create a separate build directory for libstdc++ and enter it:
+mkdir build && cd $_
+banner "Libstdc++ - Configure"; separator; confirm
+# Prepare libstdc++ for compilation:
+../libstdc++-v3/configure           \
+    --host=$LFS_TGT                 \
+    --build=$(../config.guess)      \
+    --prefix=/usr                   \
+    --disable-multilib              \
+    --disable-nls                   \
+    --disable-libstdcxx-pch         \
+    --with-gxx-include-dir=/tools/$LFS_TGT/include/c++/12.2.0
+separator
+banner "Libstdc++ - Make"; separator; confirm
+# Compile libstdc++ by running:
+make -j$(nproc)
+separator
+banner "Libstdc++ - Make Install"; separator; confirm
+# Install the library:
+make DESTDIR=$LFS install
+separator
+# Remove the libtool archive files because they are harmful for cross compilation:
+rm $LFS/usr/lib/lib{stdc++,stdc++fs,supc++}.la
+cd ../.. && rm -rf gcc*/
+
+# More details: https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter08/gcc.html#contents-gcc
